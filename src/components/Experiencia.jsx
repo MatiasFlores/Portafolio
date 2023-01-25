a8748a90da22e2f3eb123c2d40668ade2113b505
matiflores_ <matias.flores.embon@gmail.com>
2023-01-25 01:44:39 +0100

    Experiencia.jsx se quita un div que estaba de mas - Se agrega imagen de portada

--- src/components/Experiencia.jsx
+++ src/components/Experiencia.jsx
@@ -64,14 +64,14 @@ export default function Modal() {
     break;
     }
     }
+    
   return (
     <div name="Experience" className="bg-gradient-to-b from-gray-800 to-black w-full h-auto">
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
-        <div>
-          <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline position:relative;">
+          <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline position:relative">
           Experience
           </p>
-        </div>
+
 {/*DIV para el cuadro donde se muestran las imagenes */}
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
           {experiencia.map(({ id, src, title,fecha,lugar,style }) => (
